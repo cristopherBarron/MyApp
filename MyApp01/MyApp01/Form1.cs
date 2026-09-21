1ba20bb6d191c817761c5e12d049ffc607350588
cristopherBarron <alu.24131152@correo.itlalaguna.edu.mx>
2026-09-21 13:50:49 -0600

    ejemplo 1 de dd csv

--- MyApp01/MyApp01/Form1.cs
+++ MyApp01/MyApp01/Form1.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CsvHelper;
+using System.Globalization;
+using System.IO;
 
 namespace MyApp01
 {
     public partial class Form1 : Form
     {
+        List<Persona> registros = new List<Persona>();
         public Form1()
         {
             InitializeComponent();
@@ -55,6 +59,20 @@ namespace MyApp01
         private void btnCalcular_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            if (ofdCSV.ShowDialog() == DialogResult.OK)
+            { 
+                var reader = new StreamReader(ofdCSV.FileName);
+                var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+                registros = csv.GetRecords<Persona>().ToList();
+                foreach (var registro in registros)
+                {
+                    dgvRegistros.Rows.Add(registro.id,registro.name,registro.email);
+                }
+            }
         }
     }
 }
